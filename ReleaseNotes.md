32b27c17016d5dea100085e1fa120b3bfece62d6
SharpeRAD <code@sharperad.com>
2017-11-04 12:08:17 +0000

    Creating doc in net461

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,2 +1,5 @@
+### New in 0.0.2 (Released 2017/11/04)
+* [Bug] Creating doc in net461
+
 ### New in 0.0.1 (Released 2017/10/23)
 * [Feature] First release.
--- src/Cake.AWS.Lambda/Properties/Namespaces.cs
+++ src/Cake.AWS.Lambda/Properties/Namespaces.cs
@@ -1,14 +1,13 @@
 using System.Runtime.CompilerServices;
-// ReSharper disable CheckNamespace
-
 
 namespace Cake.AWS.Lambda
 {
     /// <summary>
-    /// This namespace contains Amazon Amazon Elastic Computing aliases and related members.
+    /// This namespace contains Amazon Lambda aliases and related members.
     /// </summary>
     [CompilerGenerated]
     internal class NamespaceDoc
     {
+
     }
 }
